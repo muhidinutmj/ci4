c00582d80aafcbd2fbbb47c6b9e49a0c85d366a3
Muhidin UTMJ <muhidin@utmj.ac.id>
2026-03-11 10:51:13 +0700

    Pertemuan 2

---
+++ README.md
@@ -0,0 +1,9 @@
+# Belajar CodeIgniter 4
+
+## SRK402 Pemrograman Web Berbasis Framework / STI404  Pemrograman Web Enterprise
+Belajar CI4 bersama Muhidin 
+### Pertemuan 1:
+Mampu memahami konsep dasar dari pemrograman framework
+
+### Pertemuan 2
+Mampu memahami dan menggunakan Git dan Github
---
+++ app/Views/halo_view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halo View</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Belajar View Halo di CodeIgniter</h1>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Pemrograman Web Berbasis Framework</h1>
+            <p class="col-md-8 fs-4">
+                <img src="/utmj.png" alt="kran" class="rounded-circle float-start" width="250">
+                Mata kuliah ini memberikan pemahaman dan keterampilan praktis kepada mahasiswa dalam membangun aplikasi web skala menengah hingga besar (enterprise) menggunakan framework PHP modern, yaitu Laravel dan CodeIgniter.
+            </p>
+            <button class="btn btn-primary btn-lg" type="button">
+                Selengkapnya
+            </button>
+        </div>
+    </div>
+    
+</body>
+</html>
